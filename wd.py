88f5093494b4596d56705cdb2a63beba5910945d
Botond Denes <dns.botond@gmail.com>
2018-01-06 15:59:23 +0200

    Draw size indicators

--- wd.py
+++ wd.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import collections
 import json
 import argparse
 from reportlab.pdfgen import canvas
@@ -12,6 +13,7 @@ RIGHT = 1
 BOTTOM = 2
 LEFT = 3
 BOX = (A4[0], A4[1] / 2)
+INDICATOR_HEIGHT = 0.4 * cm
 
 WINDOW_COLOR=(0, 0, 0)
 
@@ -31,16 +33,25 @@ class Rect():
     def height(self):
         return self.size[1]
 
-    def to_drawable(self):
-        return (self.top_left[0], self.top_left[1], self.size[0], self.size[1])
+    def to_drawable(self, sf):
+        return (self.top_left[0] * cm * sf, #x1
+                self.top_left[1] * cm * sf, #x2
+                self.size[0] * cm * sf, #width
+                self.size[1] * cm * sf) #height
 
-    def bottom_line(self):
-        y = self.top_left[1] + self.height
-        return (self.top_left[0], y, self.bottom_right[0], y)
+    def bottom_line(self, sf):
+        y = self.bottom_right[1] * cm * sf
+        return (self.top_left[0] * cm * sf,
+                y,
+                self.bottom_right[0] * cm * sf,
+                y)
 
-    def right_line(self):
-        x = self.top_left[0] + self.width
-        return (x, self.top_left[1], x, self.bottom_right[1])
+    def right_line(self, sf):
+        x = self.bottom_right[0] * cm * sf
+        return (x,
+                self.top_left[1] * cm * sf,
+                x,
+                self.bottom_right[1] * cm * sf)
 
     def __repr__(self):
         return str([self.top_left, self.size])
@@ -86,11 +97,11 @@ def calculate_division_rects(spec):
         for i, d in enumerate(pieces):
             w, h = r.size
             if t == "vertical":
-                s = h / n
-                d["rect"] = Rect((r.top_left[0], r.top_left[1] + i * s), (w, s))
-            elif t == "horizontal":
                 s = w / n
                 d["rect"] = Rect((r.top_left[0] + i * s, r.top_left[1]), (s, h))
+            elif t == "horizontal":
+                s = h / n
+                d["rect"] = Rect((r.top_left[0], r.top_left[1] + i * s), (w, s))
             else:
                 raise Exception("Invalid division type: `{}'".format(t))
 
@@ -98,23 +109,105 @@ def calculate_division_rects(spec):
                 calculate_division_rects(d)
 
 
-def draw_division(c, spec):
+def draw_division(c, spec, sf):
     t = spec.get("type", None)
     if t is None:
         return
 
     pieces = spec["pieces"]
-    for i, d in enumerate(pieces):
+    for i, d in enumerate(reversed(pieces)):
         r = d["rect"]
-        if i == len(pieces) - 1:
-            break
+        if i == 0:
+            continue
 
         if t == "vertical":
-            c.line(*r.bottom_line())
+            c.line(*r.right_line(sf))
         else:
-            c.line(*r.right_line())
+            c.line(*r.bottom_line(sf))
 
-        draw_division(c, d)
+        draw_division(c, d, sf)
+
+
+def calculate_division_sizes(spec, v, h, l):
+    for i, p in enumerate(spec["pieces"]):
+        r = p["rect"]
+        if spec["type"] == "vertical":
+            v[l].append(r.width)
+        else:
+            h[l].append(r.height)
+        if "type" in p:
+            calculate_division_sizes(p, v, h, l + 1)
+
+
+def calculate_sizes(spec):
+    h = collections.defaultdict(list)
+    v = collections.defaultdict(list)
+    h[0] = [spec["height"]]
+    v[0] = [spec["width"]]
+
+    if "division" in spec:
+        calculate_division_sizes(spec["division"], v, h, 1)
+
+    return h, v
+
+
+def draw_horizontal_sizes(c, spec, h, sf):
+    c.saveState()
+    c.rotate(90)
+    c.translate(0, -spec["width"] * cm * sf -1 * cm)
+    i = 0
+    for l in reversed(sorted(h.keys())):
+        c.translate(0, -i * 0.4 * cm)
+        if len(h[l]) == 0:
+            continue
+
+        i = i + 1
+        c.saveState()
+        for size in h[l]:
+            draw_size(c, size, sf)
+            c.translate(size * cm * sf, 0)
+        c.restoreState()
+
+    c.restoreState()
+
+
+def draw_size(c, size, sf):
+    text = "{:.2f}".format(size)
+    text_width = c.stringWidth(text)
+
+    s = size * cm * sf
+    x = s / 2 - text_width / 2
+    c.drawString(x, 0, text)
+    c.line(0, 0.05 * cm, 0, INDICATOR_HEIGHT  - 0.05 * cm)
+    c.line(s, 0.05 * cm, s, INDICATOR_HEIGHT  - 0.05 * cm)
+    c.line(0, INDICATOR_HEIGHT / 2, x - 0.05 * cm, INDICATOR_HEIGHT / 2)
+    c.line(x + text_width + 0.05 * cm, INDICATOR_HEIGHT / 2, s, INDICATOR_HEIGHT / 2)
+
+
+def draw_vertical_sizes(c, v, sf):
+    c.saveState()
+    c.translate(0, -1 * cm)
+    i = 0
+    for l in reversed(sorted(v.keys())):
+        c.translate(0, -i * 0.4 * cm)
+        if len(v[l]) == 0:
+            continue
+
+        i = i + 1
+        c.saveState()
+        for size in v[l]:
+            draw_size(c, size, sf)
+            c.translate(size * cm * sf, 0)
+        c.restoreState()
+
+    c.restoreState()
+
+
+def draw_sizes(c, spec, sf):
+    h, v = calculate_sizes(spec)
+
+    draw_horizontal_sizes(c, spec, h, sf)
+    draw_vertical_sizes(c, v, sf)
 
 
 def draw_window(c, spec, index_on_page):
@@ -123,27 +216,27 @@ def draw_window(c, spec, index_on_page):
     sf = scale(spec)
 
     c.translate(*translate(spec, sf, index_on_page))
-    c.scale(sf, sf)
     c.setStrokeColorRGB(*WINDOW_COLOR)
 
-    w, h = spec["width"] * cm, spec["height"] * cm
-    r = Rect((0, 0), (w, h))
+    r = Rect((0, 0), (spec["width"], spec["height"]))
 
     # Outer rectangle
-    c.rect(*r.to_drawable())
+    c.rect(*r.to_drawable(sf))
 
     spec["division"]["rect"] = r
     calculate_division_rects(spec["division"])
-    draw_division(c, spec["division"])
+    draw_division(c, spec["division"], sf)
+
+    draw_sizes(c, spec, sf)
 
     c.restoreState()
 
 def draw_windows(spec):
     c = canvas.Canvas(spec["name"] + ".pdf", pagesize=A4)
     for i, window in enumerate(spec["windows"]):
-        draw_window(c, window, i % 2)
         if i > 1 and i % 2 == 0:
             c.showPage()
+        draw_window(c, window, 1 - (i % 2))
     c.save()
 
 
